314c775cf0a7d7f715c17dd1ad9aaf142b0278d1
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-10-14 23:28:14 +0300

    Tasks D5 using prettier

--- dataTypes.js
+++ dataTypes.js
@@ -1,27 +1,35 @@
 // variables int,String, array and  object
-let x=2
-let s="Hello"
-let array=['yellow','red','green']
-car ={
-    1:"red"
-}
+let x = 2;
+let s = "Hello";
+let array = ["yellow", "red", "green"];
+car = {
+  1: "red",
+};
 
 // print variables
-console.log("This is  Int",x,"This is  String",s,"This is array",array,"This is Object",car)
+console.log(
+  "This is  Int",
+  x,
+  "This is  String",
+  s,
+  "This is array",
+  array,
+  "This is Object",
+  car
+);
 
 // display type of each variable
-console.log(typeof(x));
-console.log(typeof(s));
-console.log(typeof(array));
-console.log(typeof(car));
+console.log(typeof x);
+console.log(typeof s);
+console.log(typeof array);
+console.log(typeof car);
 
-//array of mixed values 
-let mix=[1,2,'green'] //object
-console.log(typeof(mix));
+//array of mixed values
+let mix = [1, 2, "green"]; //object
+console.log(typeof mix);
 
 // null and undifeind variables
-let b
-console.log("this is undifeind variable : ",typeof(b));
-b=null
-console.log("this is assigned to be null variable : ",typeof(b))
-
+let b;
+console.log("this is undifeind variable : ", typeof b);
+b = null;
+console.log("this is assigned to be null variable : ", typeof b);
